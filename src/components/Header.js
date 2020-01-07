191bfe98302d637b4e1117c3fc09ab4c9195eb86
LAPTOP-J5SPII67\Acer <vikrantrath4@gmail.com>
2020-01-08 04:48:31 +0530

    Structured Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,37 @@
 import React from 'react'
 export default function Header(props) {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">SH&I</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
+            <div class="col-md-2">
+                <a className="navbar-brand" href="#">SH&I</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+            </div>
+            <div className="collapse navbar-collapse col-md-8 " id="navbarNav">
+                <ul className="navbar-nav col-md-10 d-flex justify-content-between align-content-center">
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Reports</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Features</a>
+                        <a className="nav-link" href="#">Services</a>
                     </li>
                     <li className="nav-item">
                         <a className="nav-link" href="#">Pricing</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
+                        <a className="nav-link " href="#" >Tools</a>
                     </li>
+                    <li className="nav-item offset-md-2 ">
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon1" />
+                            <div class="input-group-append">
+                                <span class="input-group-text" id="basic-addon1"><i class="fa fa-search" aria-hidden="true"></i>
+                                </span>
+                            </div>
+                        </div></li>
                 </ul>
             </div>
-        </nav>
+        </nav >
     )
 }
