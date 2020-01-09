bf7cd019c8f08cd65177c3385780e6220f7f166b
LAPTOP-J5SPII67\Acer <vikrantrath4@gmail.com>
2020-01-10 03:25:54 +0530

    Changed Footer

--- src/components/Base/Footer.js
+++ src/components/Base/Footer.js
@@ -1,30 +1,27 @@
 import React from 'react'
+
+import ContactInfo from './FooterComponents/ContactInfo'
+import NewsLetter from './FooterComponents/NewsLetter'
+import ContactForm from './FooterComponents/ContactForm'
+import Hotlinks from './FooterComponents/Hotlinks'
+
+
 export default function Header() {
     return (
-        <footer className="page-footer bg-info font-small cyan darken-3" >
+        <footer className="page-footer bg-info font-small" >
             <div className="container">
-                <div className="row">
-                    <div className="col-md-12 py-5">
-                        <div className="mb-5 flex-center">
-                            <a className="fb-ic">
-                                <i className="fa fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
-                            </a>
-                            <a className="tw-ic">
-                                <i className="fa fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
-                            </a>
-                            <a className="gplus-ic">
-                                <i className="fa fa-google-plus-g fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
-                            </a>
-                            <a className="li-ic">
-                                <i className="fa fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
-                            </a>
-                            <a className="ins-ic">
-                                <i className="fa fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
-                            </a>
-                            <a className="pin-ic">
-                                <i className="fa fa-pinterest fa-lg white-text fa-2x"> </i>
-                            </a>
-                        </div>
+                <div className="row justify-content-between">
+                    <div className="col-md-3">
+                        <ContactInfo />
+                    </div>
+                    <div className="col-md-3">
+                        <NewsLetter />
+                    </div>
+                    <div className="col-md-3">
+                        <ContactForm />
+                    </div>
+                    <div className="col-md-3">
+                        <Hotlinks />
                     </div>
                 </div>
             </div>
