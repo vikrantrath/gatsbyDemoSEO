112e25adb8eae418c6351c53376c7e46a4b0cd2f
LAPTOP-J5SPII67\Acer <vikrantrath4@gmail.com>
2020-01-10 02:17:04 +0530

    Added Logo

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,9 +4,9 @@ import FeaturedSection from './HomeComponents/FeaturedSection'
 export default function Home() {
     return (
         <section id="Home">
-            <div className="container-fluid">
-                <FeaturedSection />
-            </div>
+
+            <FeaturedSection />
+
         </section>
     )
 }
