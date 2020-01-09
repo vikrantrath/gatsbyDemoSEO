ee872b5f115aab597182aad12e878244390a193f
LAPTOP-J5SPII67\Acer <vikrantrath4@gmail.com>
2020-01-10 04:11:10 +0530

    Made some responsive changes

--- src/components/Home/HomeComponents/WhyChooseUs.js
+++ src/components/Home/HomeComponents/WhyChooseUs.js
@@ -15,32 +15,32 @@ export default function () {
                 <h1 className="text-center text-uppercase font-weight-light col-md-12 mb-3">Why Choose Us</h1>
             </div>
             <div className="row">
-                <div class="text-center col-md-2">
+                <div class="text-center col-md-2 col-sm-6">
                     <img src={microscope} class="rounded mb-3" alt="..." />
                     <div className="font-weight-bold">100+</div>
                     <div className="font-weight-normal p-2">Years Of Cumulative Market Research Experience</div>
                 </div>
-                <div class="text-center col-md-2">
+                <div class="text-center col-md-2 col-sm-6">
                     <img src={globe} class="rounded mb-3" alt="..." />
                     <div className="font-weight-bold">1000+</div>
                     <div className="font-weight-normal p-2">Cross Country Market Reports</div>
                 </div>
-                <div class="text-center col-md-2">
+                <div class="text-center col-md-2 col-sm-6">
                     <img src={client1} class="rounded mb-3" alt="..." />
                     <div className="font-weight-bold">500</div>
                     <div className="font-weight-normal p-2">Clients Include Fortune 500 Companies</div>
                 </div>
-                <div class="text-center col-md-2">
+                <div class="text-center col-md-2 col-sm-6">
                     <img src={phone} class="rounded mb-3" alt="..." />
                     <div className="font-weight-bold">200+</div>
                     <div className="font-weight-normal p-2">Hours Of Free Post Sale Services</div>
                 </div>
-                <div class="text-center col-md-2">
+                <div class="text-center col-md-2 col-sm-6">
                     <img src={report} class="rounded mb-3" alt="..." />
                     <div className="font-weight-bold">1000K +</div>
                     <div className="font-weight-normal p-2">Confirmed Data Points</div>
                 </div>
-                <div class="text-center col-md-2">
+                <div class="text-center col-md-2 col-sm-6">
                     <img src={ontime} class="rounded mb-3" alt="..." />
                     <div className="font-weight-bold">300+</div>
                     <div className="font-weight-normal p-2">Ontime Delivery</div>
