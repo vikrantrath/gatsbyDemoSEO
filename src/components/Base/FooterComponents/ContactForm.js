2b3b361332cd3b7f881ef80471d507f085a5d2fd
mukesh purohit <mpurohit88@gmail.com>
2020-06-11 10:55:10 +0530

    path name change

--- src/components/Base/FooterComponents/ContactForm.js
+++ src/components/Base/FooterComponents/ContactForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 
-import './ContactForm.css';
+import './contactForm.css';
 
 export default function ContactForm() {
     const { register, handleSubmit, errors, reset } = useForm();
