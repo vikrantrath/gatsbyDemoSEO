6c4119f299a485c161d63728d7bfa8221dd60a5c
mukesh purohit <mpurohit88@gmail.com>
2020-06-11 09:29:13 +0530

    url changed

--- src/components/Base/FooterComponents/ContactForm.js
+++ src/components/Base/FooterComponents/ContactForm.js
@@ -4,7 +4,7 @@ import { post } from '../../../network/apiCalls';
 export default function ContactForm() {
     return (
         <div class="row pl-4 pr-4 d-flex align-content-around flex-wrap">
-            <form method="post" action="http://localhost:5001/api/sendEmail">
+            <form method="post" action="https://www.sheeranalyticsandinsights.com/api/sendEmail">
                 <h6 class="font-weight-bold text-white mt-2 mb-2"><ins>Submit Your Query</ins></h6>
                 <input type="text" class="form-control mb-2" placeholder="Your Name*" aria-label="Your Name*" id="name" name="name" />
                 <input type="email" class="form-control mb-2" placeholder="Your Email Address*" aria-label="Your Email Address*" id="emailId" name="emailId" />
