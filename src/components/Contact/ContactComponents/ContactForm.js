508c6d1718ebb9575c70a78b7a48f956584e409a
mukesh purohit <mpurohit88@gmail.com>
2020-06-12 12:55:56 +0530

    discount link fixed

--- src/components/Contact/ContactComponents/ContactForm.js
+++ src/components/Contact/ContactComponents/ContactForm.js
@@ -1,24 +1,52 @@
 import React from "react"
+import { useForm } from 'react-hook-form';
+
+import { post } from '../../../network/apiCalls';
+
+import './contactForm.css';
 
 export default function ContactForm() {
+  const { register, handleSubmit, errors, reset } = useForm();
+
+  const onSubmit = async data => {
+    try {
+      post(data, () => { reset(); });
+
+    } catch (error) {
+      console.log('errors....', error);
+    }
+  };
+
   return (
-    <>
+    <form onSubmit={handleSubmit(onSubmit)} type="POST">
       <h3 className="mb-5">E-MAIL FORM</h3>
       <div className="row">
         <div className="col-sm-6 mb-2">
           <input
             type="text"
-            className="form-control mb-2"
+            ref={register({
+              required: "Required",
+            })}
+            className={`form-control mb-2 ${errors.name ? ' error' : ''}`}
             placeholder="Name"
             aria-label="Your Name*"
+            name="name"
           />
         </div>
         <div className="col-sm-6 mb-2">
           <input
-            type="text"
-            className="form-control mb-2"
+            type="email"
+            ref={register({
+              required: "Required",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                message: "invalid email address"
+              }
+            })}
+            className={`form-control mb-2 ${errors.emailId ? ' error' : ''}`}
             placeholder="Email"
             aria-label="Your Email Address*"
+            name="emailId"
           />
         </div>
       </div>
@@ -26,17 +54,21 @@ export default function ContactForm() {
         <div className="col-sm-6 mb-2">
           <input
             type="text"
+            ref={register}
             className="form-control mb-2"
             placeholder="Job Title"
             aria-label="Your Name*"
+            name="jobTitle"
           />
         </div>
         <div className="col-sm-6 mb-2">
           <input
             type="text"
+            ref={register}
             className="form-control mb-2"
             placeholder="Company Name"
             aria-label="Your Name*"
+            name="companyName"
           />
         </div>
       </div>
@@ -44,20 +76,25 @@ export default function ContactForm() {
         <div className="col-sm-12 mb-2">
           <input
             type="text"
+            ref={register}
             className="form-control mb-2"
             placeholder="Mobile Number"
             aria-label="Your Contact Number"
+            name="number"
           />
         </div>
       </div>
       <textarea
         type="textarea"
+        ref={register}
         className="form-control mb-2"
         placeholder="Message"
         aria-label="Your Message"
         rows="8"
+        name="message"
       />
-
+      <input type="hidden" ref={register} value="2" id="type" name="type" />
+      <button type="button " className="btn btn-dark btn-sm btn-block btn-outline-light mt-2 mb-2">Submit</button>
       <div className="row">
         <div className="col-sm-12">
           <div
@@ -67,6 +104,6 @@ export default function ContactForm() {
           ></div>
         </div>
       </div>
-    </>
+    </form>
   )
 }
