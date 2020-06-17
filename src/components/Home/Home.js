40ddac6c22a483a9428e377880233a2e496c04ce
LAPTOP-J5SPII67\Acer <vikrantrath4@gmail.com>
2020-06-18 04:04:43 +0530

    Added paypal integration module

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,23 +1,24 @@
-import React from 'react'
-import FeaturedSection from './HomeComponents/FeaturedSection'
-import WhyChooseUs from './HomeComponents/WhyChooseUs'
-import Solutions from './HomeComponents/Solutions'
-import Industry from './HomeComponents/Industry'
-import LatestPublication from './HomeComponents/LatestPublication'
-import Benefits from './HomeComponents/Benefits'
-import Clients from './HomeComponents/Clients'
+import React from "react"
+import FeaturedSection from "./HomeComponents/FeaturedSection"
+import WhyChooseUs from "./HomeComponents/WhyChooseUs"
+import Solutions from "./HomeComponents/Solutions"
+import Industry from "./HomeComponents/Industry"
+import LatestPublication from "./HomeComponents/LatestPublication"
+import Benefits from "./HomeComponents/Benefits"
+import Clients from "./HomeComponents/Clients"
+import PaypalButton from "../Payments/PaypalButton"
 
 export default function Home() {
-    
-    return (
-        <section id="Home">
-            <FeaturedSection />
-            <Solutions />
-            <Industry />
-            <LatestPublication />
-            <Benefits />
-            <Clients />
-            <WhyChooseUs />
-        </section>
-    )
+  return (
+    <section id="Home">
+      <FeaturedSection />
+      <PaypalButton currency={"USD"} total={0.1} />
+      <Solutions />
+      <Industry />
+      <LatestPublication />
+      <Benefits />
+      <Clients />
+      <WhyChooseUs />
+    </section>
+  )
 }
---
+++ src/components/Payments/PaypalButton.js
@@ -0,0 +1,96 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import scriptLoader from "react-async-script-loader"
+
+let PayPalButton = null
+
+const CLIENT = {
+  sandbox:
+    "xxxxxx" ,//to be replaced by client id
+  production: "xxxXXX",
+}
+
+const ENV = process.env.NODE_ENV === "production" ? "production" : "sandbox"
+
+class PaypalButton extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      showButton: false,
+    }
+
+    window.React = React
+    window.ReactDOM = ReactDOM
+  }
+
+  componentDidMount() {
+    const { isScriptLoaded, isScriptLoadSucceed } = this.props
+
+    if (isScriptLoaded && isScriptLoadSucceed) {
+      PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM })
+      this.setState({ loading: false, showButton: true })
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { isScriptLoaded, isScriptLoadSucceed } = nextProps
+
+    const scriptJustLoaded =
+      !this.state.showButton && !this.props.isScriptLoaded && isScriptLoaded
+
+    if (scriptJustLoaded) {
+      if (isScriptLoadSucceed) {
+        PayPalButton = window.paypal.Buttons.driver("react", {
+          React,
+          ReactDOM,
+        })
+        this.setState({ loading: false, showButton: true })
+      }
+    }
+  }
+  createOrder = (data, actions) => {
+    return actions.order.create({
+      purchase_units: [
+        {
+          description: +"Mercedes G-Wagon",
+          amount: {
+            currency_code: data.currency,
+            value: data.total,
+          },
+        },
+      ],
+    })
+  }
+
+  onApprove = (data, actions) => {
+    actions.order.capture().then(details => {
+      const paymentData = {
+        payerID: data.payerID,
+        orderID: data.orderID,
+      }
+      console.log("Payment Approved: ", paymentData)
+      this.setState({ showButton: false, paid: true })
+    })
+  }
+
+  render() {
+    const { showButton } = this.state
+    return (
+      <div>
+        {showButton && (
+          <PayPalButton
+            createOrder={(data, actions) =>
+              this.createOrder(this.props, actions)
+            }
+            onApprove={(data, actions) => this.onApprove(data, actions)}
+          />
+        )}
+      </div>
+    )
+  }
+}
+
+export default scriptLoader(
+  `https://www.paypal.com/sdk/js?client-id=${CLIENT.sandbox}`
+)(PaypalButton)
